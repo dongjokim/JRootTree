375d78b641a7866fb68c54a463a5b55812dbd6ed
DongJo Kim <dong.jo.kim@cern.ch>
2023-01-06 12:31:32 +0200

    Update DCCI2_read.C

--- DCCI2_read.C
+++ DCCI2_read.C
@@ -4,7 +4,8 @@
 #include "TFile.h"
 #include "TTree.h"
 #include <iostream>
-
+#include <sstream>
+#include <fstream>
 using namespace std;
 int main(int argc, char **argv){
 
@@ -13,12 +14,12 @@ int main(int argc, char **argv){
 		exit(1);
 	}
 
-	char *inputfile = argv[1];
+	char *fname = argv[1];
 	char *outputfile = argv[2];
 
-	//ifstream in;
-	//in.open(inputfile);
-	FILE *pf = fopen(inputfile,"r");
+	ifstream in;
+	in.open(fname,ios::in);
+	if(!in){ return 1;}
 
 	TFile *fOutRoot = new TFile(outputfile, "RECREATE");
     TClonesArray *event = new TClonesArray("JBaseEventHeader",1000);
@@ -28,32 +29,43 @@ int main(int argc, char **argv){
 	jTree->Branch("tracks",tracks);
 
 	int  eventID;
-	int dummy1, dummy2, colortag, anticolortag, ParticleID; 
-    double mass, energy, px, py,pz, rapidity, x, y, z, t, dummy3,origin;
-
 	int nevents = 0;
 	int NTracks = 10;
-	while(!feof(pf)){
-	//int c;
-	//while ((c = getc(pf)) != '#' && c != EOF){
-		event->Clear();
-		tracks->Clear();
-		JBaseEventHeader *hdr = new( (*event)[event->GetEntriesFast()] ) JBaseEventHeader;
-		eventID = nevents;
-		hdr->SetEventID(eventID);
-			//hdr->SetEventPlane(xx)
-		cout << "eventID : " << eventID <<"\t Number of tracks ="<< NTracks<<  endl;
-		for ( int ip=0; ip<NTracks; ip++ ){
-			fscanf(pf,"%d %d %d %d %d %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %*[^\n]\n",
-					&dummy1, &dummy2, &colortag, &anticolortag, &ParticleID, &mass, &energy, &px, &py, &pz, &rapidity, &x, &y, &z, &t, &dummy3,&origin);
-			new ( (*tracks)[ip] )JBaseTrack(px, py, pz, energy, ParticleID, ParticleID, 0);
-			cout <<eventID<<"\t"<<px<<"\t"<<py<<"\t"<<pz<<"\t"<< ParticleID << endl;
+	int ip = 0;
+	std::string templine;
+	event->Clear();
+	tracks->Clear();
+	while(getline(in,templine)) {
+		if(templine.find('#')!=std::string::npos) {
+			cout <<"Just comments" << endl;
+		} else if(templine.find('%')!=std::string::npos){
+			
+				istringstream iss(templine);
+				std::string pct;
+				int iev, nv;
+				string com;
+				double weight_in, tau;
+				iss >> com >> iev >> nv >> tau >> weight_in;
+				JBaseEventHeader *hdr = new( (*event)[event->GetEntriesFast()] ) JBaseEventHeader;
+				eventID = iev;
+				hdr->SetEventID(eventID);
+				//hdr->SetEventPlane(xx)
+				cout << "eventID : " << eventID <<  endl;
+				nevents++;	
+		} else {
+				istringstream is(templine);
+				int data1, data2, ID, col, acol;
+				double m,e,px,py,pz,x,y,z,t,ft, rap;
+				std::string TAG;
+				is >> data1 >> data2 >> col >> acol >> ID >> m >> e >> px >> py >> pz >> rap >> x >> y >> z >> t >> ft >> TAG;
+			    new ( (*tracks)[ip++] )JBaseTrack(px, py, pz, e, ID, ID, 0);
+			    cout <<eventID<<" "<< ip<< "\t"<<px<<"\t"<<py<<"\t"<<pz<<"\t"<< ID << endl;
 		}
 		jTree->Fill(); // fill last event
-		nevents++;
 	}
-
-	fclose(pf);
+	cout <<eventID<<"\t"<< tracks->GetEntriesFast() << endl;
+	
+	//fin.close();
 
 	cout <<"Total # of events = "<< nevents << endl;
 	jTree->Write();
