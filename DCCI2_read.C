77b4779d1a2a0e6521eb8171a425630853392760
Maxim Virta <maxim.m.o.virta@jyu.fi>
2023-01-13 14:04:04 +0200

    tracks fixed

--- DCCI2_read.C
+++ DCCI2_read.C
@@ -27,7 +27,7 @@ int main(int argc, char **argv){
 	TClonesArray *tracks = new TClonesArray("JBaseTrack",100000);
 	TTree *jTree = new TTree("jTree","Tree from DCCI2 files");
 	jTree->Branch("JEventHeaderList",&event);
-	jTree->Branch("tracks",tracks);
+	jTree->Branch("JTrackList",tracks);
 
 	int ieout = 100;
 	if (ieout<1) ieout=1;
@@ -38,22 +38,32 @@ int main(int argc, char **argv){
 	int ip = 0;
 	std::string templine;
 	event->Clear();
-	tracks->Clear();
 	while(getline(in,templine)) {
-		if(templine.find('#')!=std::string::npos) {
-		} else if(templine.find('%')!=std::string::npos){
+		if ( templine.find('%')!=std::string::npos) {
+			istringstream isss(templine);
+			int tevent;
+			string com;
+			isss >> com >> tevent;
+			if (tevent>0) {jTree->Fill();};
+
+		} else if(templine.find('#')!=std::string::npos){
 			istringstream iss(templine);
 			std::string pct;
 			int iev, nv;
 			string com;
 			double weight_in, tau;
 			iss >> com >> iev >> nv >> tau >> weight_in;
-			JBaseEventHeader *hdr = new( (*event)[event->GetEntriesFast()] ) JBaseEventHeader;
-			eventID = iev;
-			hdr->SetEventID(eventID);
-			//hdr->SetEventPlane(xx)
-			if(eventID % ieout == 0) cout << "eventID : " << eventID <<  endl;
-			nevents++;	
+			if (iev<49999) {
+				JBaseEventHeader *hdr = new( (*event)[event->GetEntriesFast()] ) JBaseEventHeader;
+				eventID = iev-1;
+				hdr->SetEventID(eventID);
+				//hdr->SetEventPlane(xx)
+				if(eventID % ieout == 0) cout << "eventID : " << eventID << "\t# tracks: " << nv << endl;
+				tracks->Clear();
+				ip = 0;
+				nevents++;				
+			}
+	
 		} else {
 			istringstream is(templine);
 			int data1, data2, ID, col, acol;
@@ -63,9 +73,8 @@ int main(int argc, char **argv){
 			new ( (*tracks)[ip++] )JBaseTrack(px, py, pz, e, ID, ID, 0);
 			if(eventID % ieout == 0) cout <<"\t"<< eventID<<" "<< ip<< "\t"<<px<<"\t"<<py<<"\t"<<pz<<"\t"<< ID << endl;
 		}
-		jTree->Fill(); // fill last event
 	}
-	cout <<"Total events = "<< tracks->GetEntriesFast() << endl;
+	cout <<"Total events = "<<event->GetEntriesFast() << endl;
 
 	//fin.close();
 
