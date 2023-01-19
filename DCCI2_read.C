ef33dd82dcaa0546ce81dfa5e6de20277d1c841b
yuuka-kanakubo <y-kanakubo-75t@eagle.sophia.ac.jp>
2023-01-19 20:02:41 +0900

    DCCI2_read.C: IF statement in l.56 has changed.
     Previous IF statement, if(iev<49999), does not work to pick up event information
     at the top of each event after iev=50000. Changed the IF statement so that
     one can read the event information (second line starting from # at each event).

--- DCCI2_read.C
+++ DCCI2_read.C
@@ -23,13 +23,13 @@ int main(int argc, char **argv){
 	if(!in){ return 1;}
 
 	TFile *fOutRoot = new TFile(outputfile, "RECREATE");
-	TClonesArray *event = new TClonesArray("JBaseEventHeader",1000);
+	TClonesArray *event = new TClonesArray("JBaseEventHeader",250000);
 	TClonesArray *tracks = new TClonesArray("JBaseTrack",100000);
 	TTree *jTree = new TTree("jTree","Tree from DCCI2 files");
 	jTree->Branch("JEventHeaderList",&event);
 	jTree->Branch("JTrackList",tracks);
 
-	int ieout = 10;
+	int ieout = 12000;
 	if (ieout<1) ieout=1;
 	TStopwatch timer;
 	timer.Start();
@@ -38,6 +38,14 @@ int main(int argc, char **argv){
 	int ip = 0;
 	std::string templine;
 	event->Clear();
+
+	//We want to get the second line starting from # at the top of each event files.
+	//I would like to set EventInfoHead = true when it detects the second #, 
+	//and avoid reading the first one.
+	//Yuuka
+	//==================================================================
+	bool EventInfoHead=false;
+	int CountHush=0;
 	while(getline(in,templine)) {
 		if ( templine.find('%')!=std::string::npos) {
 			istringstream isss(templine);
@@ -45,15 +53,19 @@ int main(int argc, char **argv){
 			string com;
 			isss >> com >> tevent;
 			if (tevent>0) {jTree->Fill();};
-
+			EventInfoHead=false;
+			CountHush=0;
 		} else if(templine.find('#')!=std::string::npos){
+			CountHush++;
+			if(CountHush==2) EventInfoHead=true;
 			istringstream iss(templine);
 			std::string pct;
 			int iev, nv;
 			string com;
 			double weight_in, tau;
 			iss >> com >> iev >> nv >> tau >> weight_in;
-			if (iev<49999) {
+			if (EventInfoHead) {
+			//if (iev<49999) {
 				JBaseEventHeader *hdr = new( (*event)[event->GetEntriesFast()] ) JBaseEventHeader;
 				eventID = iev-1;
 				hdr->SetEventID(eventID);
@@ -71,7 +83,7 @@ int main(int argc, char **argv){
 			std::string TAG;
 			is >> data1 >> data2 >> col >> acol >> ID >> m >> e >> px >> py >> pz >> rap >> x >> y >> z >> t >> ft >> TAG;
 			new ( (*tracks)[ip++] )JBaseTrack(px, py, pz, e, ID, ID, 0);
-			if(eventID % ieout == 0) cout <<"\t"<< eventID<<" "<< ip<< "\t"<<px<<"\t"<<py<<"\t"<<pz<<"\t"<< ID << endl;
+			//if(eventID % ieout == 0) cout <<"\t"<< eventID<<" "<< ip<< "\t"<<px<<"\t"<<py<<"\t"<<pz<<"\t"<< ID << endl;
 		}
 	}
 	cout <<"Total events = "<<event->GetEntriesFast() << endl;
